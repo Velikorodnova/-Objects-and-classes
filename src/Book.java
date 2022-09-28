e39c9c4a70fd50f321a0a17ae6e40b3c48a31b10
hjf <marinaff-xiii@mail.ru>
2022-09-29 02:22:03 +0500

    Objects and classes

---
+++ src/Book.java
@@ -0,0 +1,30 @@
+public class Book {
+    String bookName;
+    String author;
+    int publicationYear;
+
+    public Book(String bookName, String author, int publicationYear) {
+        this.bookName = bookName;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getPublicationYear() {
+        return this.publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+}
+
+
